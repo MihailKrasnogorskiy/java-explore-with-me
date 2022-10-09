de678ab9e660d413546165cbd1f83ba1f7bde53e
Mihail Krasnogorskiy <mikser256@yandex.ru>
2022-10-09 21:26:58 +0300

    исправлено замечание по checkStyle

--- statistic/src/main/java/ru/yandex/practicum/statistic/StatRepository.java
+++ statistic/src/main/java/ru/yandex/practicum/statistic/StatRepository.java
@@ -41,7 +41,6 @@ public interface StatRepository extends CrudRepository<EndpointHit, Long> {
     @Query(value = "SELECT COUNT(uri) " +
             "from hits " +
             "where uri = ?3 " +
-            "and views_date >= ?1 and views_date<= ?2 "
-            , nativeQuery = true)
+            "and views_date >= ?1 and views_date<= ?2 ", nativeQuery = true)
     Long getView(LocalDateTime startTime, LocalDateTime endTime, String uri);
 }
